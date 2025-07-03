b6990d703d6086a468f758877c4abdc89d410dcd
VolodymyrZin <vl.zinych@gmail.com>
2025-07-03 22:58:36 +0300

    add.index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Slider</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="slider">
+    <button id="prev">&#9664;</button>
+    <img id="slider-image" src="images/1.jpg" alt="slide">
+    <button id="next">&#9654;</button>
+</div>
+<script src="js/script.js" defer></script>
+</body>
+</html>
+
